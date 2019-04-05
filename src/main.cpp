26eee74c092fd17b15d441f9131fc9091b02db47
Or Fleisher <fleisher.or@gmail.com>
2019-04-05 00:02:43 -0400

    Adding basic flow for setting up the stream connection

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,23 @@
 using namespace std;
 using namespace RTMP;
 
+#define VIMEO_LIVE_RTMP_ENDPOINT "rtmp://rtmp-global.cloud.vimeo.com/live"
+
 int main()
 {
-    cout << endl << "Welcome to RTMP streamer 📽" << endl << "Written by @juniorxsound <https://orfleisher.com>" << endl << endl;
-    
+    cout << endl
+         << "Welcome to RTMP streamer 📽" << endl
+         << "Written by @juniorxsound <https://orfleisher.com>" << endl
+         << endl;
+
     Streamer streamer;
-    streamer.enable_av_debug_log();
+    // streamer.enable_av_debug_log();
+
+    Config stream_config(640, 360,
+                         640, 360,
+                         30, 50000, "high444", (string)VIMEO_LIVE_RTMP_ENDPOINT + "/stream-key");
+
+    streamer.Init(stream_config);
 
     return 0;
 }
